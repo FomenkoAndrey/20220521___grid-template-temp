b3b12c07b6920cdb334f17bbfe570fa0c76607cf
Alecto <fomenkoandrey1978@gmail.com>
2022-05-22 14:02:04 +0300

    начали использовать меди-миксины, работает на отзывчивостью.

--- gulpfile.js
+++ gulpfile.js
@@ -30,25 +30,15 @@ const PLUGINS = [
 ];
 
 function scss() {
-  return src(PATH.scssFile).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(PLUGINS)).
-    pipe(dest(PATH.cssFolder)).
-    pipe(browserSync.stream());
+  return src(PATH.scssFile).pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).pipe(postcss(PLUGINS)).pipe(dest(PATH.cssFolder)).pipe(browserSync.stream());
 }
 
 function scssDev() {
-  return src(PATH.scssFile, {sourcemaps: true}).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(PLUGINS)).
-    pipe(dest(PATH.cssFolder, {sourcemaps: true})).
-    pipe(browserSync.stream());
+  return src(PATH.scssFile, {sourcemaps: true}).pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).pipe(postcss(PLUGINS)).pipe(dest(PATH.cssFolder, {sourcemaps: true})).pipe(browserSync.stream());
 }
 
 function comb() {
-  return src(PATH.scssFiles).
-    pipe(csscomb()).
-    pipe(dest(PATH.scssFolder));
+  return src(PATH.scssFiles).pipe(csscomb()).pipe(dest(PATH.scssFolder));
 }
 
 function syncInit() {
